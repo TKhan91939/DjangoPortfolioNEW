3405b72b0432d0619d4865ad5d60dfb41f49ea82
Abdurrahman Habeeb <110705639+Ahabeeb1230@users.noreply.github.com>
2026-04-05 17:55:58 -0400

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
+# core/urls.py
 from django.urls import path 
 from . import views
 
 urlpatterns = [
-    # This now handles http://djangoportfolionew-production.up.railway.app/
-    path("", views.project_list, name="projects"), 
+    path("", views.project_list, name="projects"),         # Works at /
+    path("projects/", views.project_list, name="projects_list"), # Works at /projects/
     
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),

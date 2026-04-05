472dcc4c9535a19b6260747f3a30a9fd65070c50
Abdurrahman Habeeb <110705639+Ahabeeb1230@users.noreply.github.com>
2026-04-05 17:51:37 -0400

    Update urlpatterns to handle root URL

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("projects/", views.project_list, name="projects"),
+    # This now handles http://djangoportfolionew-production.up.railway.app/
+    path("", views.project_list, name="projects"), 
+    
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
 ]
